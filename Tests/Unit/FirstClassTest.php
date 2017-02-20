cad4e374df94e6951be3cccb55625b32bc082dd6
Maik Kempe <mkempe@bitaculous.com>
2017-02-20 07:13:36 +0100

    [➠] Updated comments [ci skip].

--- Tests/Unit/FirstClassTest.php
+++ Tests/Unit/FirstClassTest.php
@@ -5,6 +5,9 @@ use \TYPO3\CMS\Core\Tests\UnitTestCase;
 
 use \T3v\T3vDataMapper\Tests\Unit\Fixtures\LoadableClass;
 
+/**
+ * First Class Test Class
+ */
 class FirstClassTest extends UnitTestCase {
   /**
    * Test if method returns true.
--- Tests/Unit/Fixtures/LoadableClass.php
+++ Tests/Unit/Fixtures/LoadableClass.php
@@ -1,6 +1,9 @@
 <?php
 namespace T3v\T3vDataMapper\Tests\Unit\Fixtures;
 
+/**
+ * Loadable Class
+ */
 class LoadableClass {
   public function returnsTrue() {
     return true;
