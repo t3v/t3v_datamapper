b05d37d94a0caab217ebd8cccfd85349c439520e
Maik Kempe <mkempe@bitaculous.com>
2017-05-24 17:10:17 +0200

    [➠] Updated scripts.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,11 @@ Changelog
 
 Notable changes will be documented in this file. The project adheres to [Semantic Versioning].
 
+Unreleased
+----------
+
+* Updated scripts
+
 2.0.1
 -----
 
--- Scripts/Setup.sh
+++ Scripts/Setup.sh
@@ -6,6 +6,11 @@ TYPO3_VERSION="^8.7"
 
 # === Commands ===
 
+# Remove Composer lock file if available
+if [ -f composer.lock ]; then
+  rm composer.lock
+fi
+
 # Install TYPO3 and all other required dependencies
 composer require typo3/cms="$TYPO3_VERSION"
 
