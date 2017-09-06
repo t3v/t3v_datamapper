69f4759c02d000a52b79a39788a7f4a582c28718
Maik Kempe <mkempe@bitaculous.com>
2017-09-06 18:51:51 +0200

    [♻] Cleaned up.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,7 @@
 <?php
 defined('TYPO3_MODE') or die('Access denied.');
 
-call_user_func(function ($namespace, $extkey) {
+call_user_func(function($namespace, $extkey) {
   $extensionSignature = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($namespace . '.' . $extkey);
 
   // === Commands ===
