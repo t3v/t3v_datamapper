857dc3782f4d4fbc183f517a87c054536c50b99c
Maik Kempe <mkempe@bitaculous.com>
2017-11-14 17:26:51 +0100

    [➠] Refactored TypoScript.

--- Configuration/TypoScript/Vendor/constants.ts
+++
@@ -1,3 +0,0 @@
-# === Includes ===
-
-<INCLUDE_TYPOSCRIPT: source="FILE: ./T3vDataMapper/constants.ts">
--- Configuration/TypoScript/Vendor/setup.ts
+++
@@ -1,3 +0,0 @@
-# === Includes ===
-
-<INCLUDE_TYPOSCRIPT: source="FILE: ./T3vDataMapper/setup.ts">
