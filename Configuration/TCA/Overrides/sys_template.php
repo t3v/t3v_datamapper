5a5d8469c8fc31f94611cb7802bec9bd1ec22ca9
Maik Kempe <mkempe@bitaculous.com>
2018-03-08 14:26:30 +0100

    [♻] Cleaned up.

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -3,8 +3,9 @@ defined('TYPO3_MODE') or die();
 
 // === Variables ===
 
-$extensionKey = 't3v_datamapper';
+$extensionKey   = 't3v_datamapper';
+$extensionTitle = 'T3v DataMapper';
 
 // === TypoScript ===
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extensionKey, 'Configuration/TypoScript', 'T3v DataMapper');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extensionKey, 'Configuration/TypoScript', $extensionTitle);
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,9 +3,7 @@ defined('TYPO3_MODE') or die();
 
 // === Variables ===
 
-$namespace          = 't3v';
-$extensionKey       = $_EXTKEY;
-$extensionSignature = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($namespace . '.' . $extensionKey);
+$extensionKey = $_EXTKEY;
 
 // === Extbase Commands ===
 
