68d9f98f02a0352286ef448c607d49e941e8cf8a
Maik Kempe <mkempe@bitaculous.com>
2018-06-17 19:55:18 +0200

    [♻] Cleaned up.

--- Tests/Functional/RenderingTest.php
+++ Tests/Functional/RenderingTest.php
@@ -29,7 +29,7 @@ class RenderingTest extends FunctionalTestCase {
   protected $testExtensionsToLoad = ['typo3conf/ext/t3v_datamapper'];
 
   /**
-   * Test if template is rendered.
+   * Tests if the template is rendered.
    *
    * @test
    */
