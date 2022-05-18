b7710720267ee126fd11be108c15900dae492030
Maik Kempe <mkempe@bitaculous.com>
2022-05-18 22:18:43 +0200

    [➠] Added content model.

---
+++ Classes/Domain/Model/Content.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace T3v\T3vDataMapper\Domain\Model;
+
+/**
+ * The content class.
+ *
+ * @package T3v\T3vDataMapper\Domain\Model
+ */
+class Content extends BaseModel
+{
+    /**
+     * The name of the table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'tt_content';
+}
